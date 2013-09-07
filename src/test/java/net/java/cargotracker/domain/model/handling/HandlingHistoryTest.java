227a229afa5a29b710b0830a73a6a44712e99597
reza_rahman <reza_rahman@a7a8e4b7-0737-e22c-e7e5-8ade37a62208>
2013-09-07 19:42:45 +0000

    Cleanup/complete unit tests.

--- src/test/java/net/java/cargotracker/domain/model/handling/HandlingHistoryTest.java
+++ src/test/java/net/java/cargotracker/domain/model/handling/HandlingHistoryTest.java
@@ -1,8 +1,10 @@
 package net.java.cargotracker.domain.model.handling;
 
+import static org.junit.Assert.*;
+
 import java.util.Arrays;
-import static java.util.Arrays.asList;
 import java.util.Date;
+
 import net.java.cargotracker.application.util.DateUtil;
 import net.java.cargotracker.domain.model.cargo.Cargo;
 import net.java.cargotracker.domain.model.cargo.RouteSpecification;
@@ -10,39 +12,40 @@ import net.java.cargotracker.domain.model.cargo.TrackingId;
 import net.java.cargotracker.domain.model.location.SampleLocations;
 import net.java.cargotracker.domain.model.voyage.Voyage;
 import net.java.cargotracker.domain.model.voyage.VoyageNumber;
+
 import org.junit.Test;
 
 //TODO This set of tests is very trivial, consider removing them.
 public class HandlingHistoryTest {
 
-    Cargo cargo = new Cargo(new TrackingId("ABC"), new RouteSpecification(
-            SampleLocations.SHANGHAI, SampleLocations.DALLAS,
-            DateUtil.toDate("2009-04-01")));
-    Voyage voyage = new Voyage.Builder(new VoyageNumber("X25"),
-            SampleLocations.HONGKONG).addMovement(SampleLocations.SHANGHAI,
-            new Date(), new Date()).addMovement(SampleLocations.DALLAS,
-            new Date(), new Date()).build();
-    HandlingEvent event1 = new HandlingEvent(cargo, DateUtil.toDate("2009-03-05"),
-            new Date(100), HandlingEvent.Type.LOAD,
-            SampleLocations.SHANGHAI, voyage);
-    HandlingEvent event1duplicate = new HandlingEvent(cargo,
-            DateUtil.toDate("2009-03-05"), new Date(200),
-            HandlingEvent.Type.LOAD, SampleLocations.SHANGHAI, voyage);
-    HandlingEvent event2 = new HandlingEvent(cargo, DateUtil.toDate("2009-03-10"),
-            new Date(150), HandlingEvent.Type.UNLOAD,
-            SampleLocations.DALLAS, voyage);
-    HandlingHistory handlingHistory = new HandlingHistory(asList(event2,
-            event1, event1duplicate));
+	Cargo cargo = new Cargo(new TrackingId("ABC"), new RouteSpecification(
+			SampleLocations.SHANGHAI, SampleLocations.DALLAS,
+			DateUtil.toDate("2009-04-01")));
+	Voyage voyage = new Voyage.Builder(new VoyageNumber("X25"),
+			SampleLocations.HONGKONG)
+			.addMovement(SampleLocations.SHANGHAI, new Date(), new Date())
+			.addMovement(SampleLocations.DALLAS, new Date(), new Date())
+			.build();
+	HandlingEvent event1 = new HandlingEvent(cargo,
+			DateUtil.toDate("2009-03-05"), new Date(100),
+			HandlingEvent.Type.LOAD, SampleLocations.SHANGHAI, voyage);
+	HandlingEvent event1duplicate = new HandlingEvent(cargo,
+			DateUtil.toDate("2009-03-05"), new Date(200),
+			HandlingEvent.Type.LOAD, SampleLocations.SHANGHAI, voyage);
+	HandlingEvent event2 = new HandlingEvent(cargo,
+			DateUtil.toDate("2009-03-10"), new Date(150),
+			HandlingEvent.Type.UNLOAD, SampleLocations.DALLAS, voyage);
+	HandlingHistory handlingHistory = new HandlingHistory(Arrays.asList(event2,
+			event1, event1duplicate));
 
-    @Test
-    public void testDistinctEventsByCompletionTime() {
-        org.junit.Assert.assertEquals(Arrays.asList(event1, event2),
-                handlingHistory.getDistinctEventsByCompletionTime());
-    }
+	@Test
+	public void testDistinctEventsByCompletionTime() {
+		assertEquals(Arrays.asList(event1, event2),
+				handlingHistory.getDistinctEventsByCompletionTime());
+	}
 
-    @Test
-    public void testMostRecentlyCompletedEvent() {
-        org.junit.Assert.assertEquals(event2,
-                handlingHistory.getMostRecentlyCompletedEvent());
-    }
+	@Test
+	public void testMostRecentlyCompletedEvent() {
+		assertEquals(event2, handlingHistory.getMostRecentlyCompletedEvent());
+	}
 }
